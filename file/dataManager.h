cd05cf907e5457020df5a1eec2d8a8dcb89ab4a5
paolovelove <pao.thongmotai@gmail.com>
2014-05-15 00:49:12 +0700

    Update dataManager.h

--- file/dataManager.h
+++ file/dataManager.h
@@ -2,7 +2,8 @@
  * write all of project's data to output file.
  *
  * Arguments 
- * 		outputFile - output filename
+ * 		outputFile - char *outputFile output filename
+ *				   - void* pProject pointer to project structer information
  * Returns : 1 for success, 0 for failure.
  */
 int writeData(char *outputFile,void* pProject);
@@ -10,8 +11,9 @@ int writeData(char *outputFile,void* pProject);
 /* readData
  * read the data from files to store in the programe
  * Arguments 
- * 		outputFile - output filename
- *
+ * 		outputFile - char *inputFile input filename
+ *				   - void* pProject pointer to project structer information
+ *				   - int showLog 1 is showlog 0 is none
  * Returns : 1 for success, 0 for failure.
  */
 int readData(char *inputFile,void* pProject,int showLog);
